3db5664478f9d64877a67cfab3fcb23ee50d0296
ZZJJWarth <2678328250@qq.com>
2025-02-18 15:35:40 +0800

    fmt

--- modules/ruxhal/src/arch/riscv/trap.rs
+++ modules/ruxhal/src/arch/riscv/trap.rs
@@ -43,18 +43,16 @@ fn riscv_trap_handler(tf: &mut TrapFrame, _from_user: bool) {
                 ],
             );
             tf.regs.a0 = ret as _;
-        },
+        }
         #[cfg(feature = "paging")]
-        Trap::Exception(E::LoadPageFault) =>{
-            let vaddr =riscv::register::stval::read();
-            crate::trap::handle_page_fault(vaddr,crate::trap::PageFaultCause::READ);
-
-        },
+        Trap::Exception(E::LoadPageFault) => {
+            let vaddr = riscv::register::stval::read();
+            crate::trap::handle_page_fault(vaddr, crate::trap::PageFaultCause::READ);
+        }
         #[cfg(feature = "paging")]
-        Trap::Exception(E::StorePageFault)=>{
-            let vaddr =riscv::register::stval::read();
-            crate::trap::handle_page_fault(vaddr,crate::trap::PageFaultCause::WRITE);
-
+        Trap::Exception(E::StorePageFault) => {
+            let vaddr = riscv::register::stval::read();
+            crate::trap::handle_page_fault(vaddr, crate::trap::PageFaultCause::WRITE);
         }
 
         _ => {
